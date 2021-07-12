850d30907e26819b3d1caa57996e050a6d10b87c
ÁÑÑÍHÌLÅTØR SPÄRK <tanmoyomg7@gmail.com>
2021-07-12 22:01:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
-bot_token = "16901971:AAFqdM_SQE1PB2P1xLr67k"
-ARQ_API_KEY = "Get this from @ARQBot"
-
+bot_token = "wtf" # yourbot token
+ARQ_API_KEY = "sexmmm" # Get this from @ARQBot
+api_id = 6 # your api id
+api_hash = "69696969696sexm" # your apihash
 # List of supported languages >>
 # https://py-googletrans.readthedocs.io/en/latest/#googletrans-languages
-
+# Leave it as it is
 LANGUAGE = "en"
-
 # Leave it as it is
 ARQ_API_BASE_URL = "https://thearq.tech"
