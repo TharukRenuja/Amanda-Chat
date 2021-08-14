ab7515df458ea426711abbcc9d406e09a4a1de8d
ÁÑÑÍHÌLÅTØR SPÄRK <75305464+annihilatorrrr@users.noreply.github.com>
2021-08-15 03:02:55 +0530

    Update and rename config.py to sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -1,6 +1,7 @@
-from os import environ
+from dotenv import load_dotenv
 
 ENV = True # make it false for heruko
+load_dotenv("config.env")
 
 # List of supported languages >>
 # https://py-googletrans.readthedocs.io/en/latest/#googletrans-languages
@@ -9,15 +10,16 @@ if not ENV:
   bot_token = "16901971:AAFqdM_SQE1PB2P1xLr67k"
   ARQ_API_KEY = "Get this from @ARQRobot"
   LANGUAGE = "en"
-  api_id =  # your api id
-  api_hash = "" # your apihash
+  ID =  # your api id
+  HASH = "" # your apihash
   # Leave it as it is
   ARQ_API_BASE_URL = "https://thearq.tech"
 else:
-  bot_token = str(environ.get("bot_token", None))
+  from os import environ
+  bot_token = str(environ.get("TOKEN", None))
   ARQ_API_KEY = str(environ.get("ARQ_API_KEY", None))
   LANGUAGE = str(environ.get("LANGUAGE", "en"))
-  api_id = int(environ.get("api_id", 6)) # your api id
-  api_hash = str(environ.get("api_hash", "eb06d4abfb49dc3eeb1aeb98ae0f581e")) # your apihash
+  ID = int(environ.get("ID", 6)) # your api id
+  HASH = str(environ.get("HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e")) # your apihash
   # Leave it as it is
   ARQ_API_BASE_URL = str(environ.get("ARQ_API_BASE_URL", None))
